edc112c2c92ddb4d5920803ccbe18b2bb0da60e0
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-04-02 19:03:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Simple Calculator
+
+a = int(input("put first number= "))
+b = int(input("put second number= "))
+c = input("enter the operator: ")
+
+if c == "+":
+  print("result for a + b is", a + b)
+
+if c == "-":
+  print("result for a - b is", a - b)
+
+if c == "*":
+  print("result for a * b is", a * b)
+
+if c == "/":
+  print("result for a / b is", a / b)
